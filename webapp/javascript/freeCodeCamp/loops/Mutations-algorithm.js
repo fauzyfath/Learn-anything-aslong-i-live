79d835dd6be11224d16c2f4b5fc46834e8f13516
fauzyfat <fauzy.fyz1@gmail.com>
2026-05-07 22:45:12 +0800

    Mutation Algorithm LAB (FCC)

--- webapp/javascript/freeCodeCamp/loops/Mutations-algorithm.js
+++ webapp/javascript/freeCodeCamp/loops/Mutations-algorithm.js
@@ -2,16 +2,14 @@ const mutation = array =>{
   let countCorrect = 0;
   let arrayElm2 = array[1].length;
 
-  for(let i = 0; i<array[0].length; i++){
-    let temp = array[0][i];
+  for(let i = 0; i<array[1].length; i++){
+    let temp = array[1][i];
     // console.log(temp);
 
-    for(let j=0; j<array[1].length; j++){
-      let elm2 = array[1][j]
-      console.log(elm2);
+    for(let j=0; j<array[0].length; j++){
+      let elm2 = array[0][j]
       if(temp.toLowerCase() === elm2.toLowerCase()){
         countCorrect++;
-        console.log(countCorrect);
         break;
       }
     }
