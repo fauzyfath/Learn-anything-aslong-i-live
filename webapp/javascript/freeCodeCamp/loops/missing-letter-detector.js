09434ddebfe20a6a6f06f28aa14b270be0fc9521
fauzyfat <fauzy.fyz1@gmail.com>
2026-05-12 15:35:04 +0800

    Missing Letter Detector LAB (FCC)

--- webapp/javascript/freeCodeCamp/loops/missing-letter-detector.js
+++ webapp/javascript/freeCodeCamp/loops/missing-letter-detector.js
@@ -1,28 +1,24 @@
 const fearNotLetter = string => {
   const alphabet = "abcdefghijklmnopqrstuvwxyz";
   let lostLetter = "";
-  let indexStart = 0;
+  let indexStart = alphabet.indexOf(string[0]);
 
-  for(let i =0; i<alphabet.length; i++){    
-    if(alphabet[i] === string[0]){
-      
-      indexStart = i;   //indexstart 1 ->bcdf; || indexStart 18 -> stvwx 
+  // console.log(indexStart);
 
-      for(let j = 0; j<string.length; j++){     
-        console.log(indexStart);
-        console.log(string[j]);
-        if(string[j] !== alphabet[indexStart]){
-          console.log(string[j]);
-          console.log(j+1);
-          lostLetter = alphabet[indexStart+j+1];
-          break;
-        }
-      }
+  for(let i=0; i<alphabet.length; i++){
+    // console.log(string[i]);
+    // console.log(alphabet[indexStart]);
+    // console.log("-----------")
+
+    if(string[i] !== alphabet[indexStart]){
+      // console.log(string[i]);
+      // console.log(alphabet[indexStart])
+      lostLetter = alphabet[indexStart];
+      return lostLetter
     }
-    
+    indexStart++;
   }
-  return lostLetter;
+  
 }
-console.log(fearNotLetter("bcdf"))
-
+console.log(fearNotLetter("abce"))
 
