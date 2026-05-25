334de62d0debc9639be6503345adc11a185a3e2e
fauzyfat <fauzy.fyz1@gmail.com>
2026-05-26 03:12:15 +0800

    Element skipper LAB (FCC) not satisfied with the result

---
+++ webapp/javascript/freeCodeCamp/fundamentals-review/element-skipper.js
@@ -0,0 +1,21 @@
+const dropElements = (arr, func) => {
+  let firstCorrect = null;
+  let lastCorrect = null;
+  let counter = 0;
+
+  for(let i = 0; i<arr.length; i++){
+    if(func(arr[i]) === true){
+      if(counter < 1){
+        firstCorrect = i;
+        counter++
+      }
+      lastCorrect = i+2;
+      counter++
+    }
+    continue;
+  }
+  // console.log(lastCorrect)
+  return arr.slice(firstCorrect, lastCorrect)
+}
+
+console.log(dropElements([1, 2, 3, 9, 2], function(n) {return n > 2;}))
